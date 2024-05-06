df1e78d9c87c2fc4f9fc8c5d5dc1ce6842d03c98
AndyEnvFr <andrea.ingrosso.campi@gmail.com>
2024-05-06 14:05:16 +0200

    haz

--- test_andy.py
+++ test_andy.py
@@ -1,5 +1,13 @@
 # test andy run final model
 
-(P_data, R_data, T_data, lai_data, cs, alpha,
-        gamma, beta, c_m, et_weight):
+# P_data, R_data, T_data, lai_data, cs, alpha, gamma, beta, c_m, et_weight
+
+# read in input data
+
+
+
+
+
+
+
 time_evolution()
